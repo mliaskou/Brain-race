3050e79f70dca98739f28504733c4ddb79fe8de6
mliaskou <81467679+mliaskou@users.noreply.github.com>
2021-09-17 12:10:52 +0300

    Correcting obstacles

--- Assets/Obstacle.cs
+++ Assets/Obstacle.cs
@@ -7,13 +7,22 @@ public class Obstacle : MonoBehaviour
 {
     public PointsScore pointsScore;
     public Text txt;
+    public GameObject obstacle;
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name == "car1")
         {
+            obstacle.SetActive(true);
             pointsScore.DecreaseObstaclePoints();
             txt.text = "You hit an obstacle";
         }
     }
+
+    private void OnCollisionExit(Collision other)
+    {
+        obstacle.SetActive(false);
+
+    }
+
 }
